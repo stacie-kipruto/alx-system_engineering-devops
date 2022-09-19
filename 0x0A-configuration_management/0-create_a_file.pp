4990f5a63eec5c3d8d308f77803e3b53afefe166
stacie-kipruto <staciekipruto@gmail.com>
2022-09-19 10:50:35 -0700

    create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# create file in `/tmp`
+# Using puppet, create a file with the below specifications
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
